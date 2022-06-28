3012b479889d6939e6118464bcfd6fafb885c554
Ylva Karlberg <ylvakarlberg@gmail.com>
2022-06-28 23:57:13 +0200

    added some randomness

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
+from random import randint
 import discord
 import os
 
 client = discord.Client()
 
 emoji_id = "<:hjelp:705824146046844982>" 
-triggers = {"bandy", "boll", "basket", "hockey", "mac", "macos", "linux", "ubuntu", "destiny", "elden ring", "kaffe", "stockholm", "njuta av livet", "paradox"}
-white_boy_rpg = {"witcher", "fallout","deus ex", "vampire: the masquerade", "divinity", "dragon age", "star wars: knights"}
+triggers = {"bandy", "boll", "basket", "hockey", "mac", "macos", "linux", "ubuntu", "destiny", "elden ring", "kaffe", "stockholm", "njuta av livet", "paradox", "träning"}
+white_boy_rpg = {"witcher", "fallout", "new vegas", "deus ex", "vampire: the masquerade", "divinity", "dragon age", "star wars: knights"}
 response = ""
 
 @client.event
@@ -17,17 +18,23 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    for trigger in triggers:
-        if trigger in message.content.lower():
-            if trigger == "boll":
-                response = "bollsport"
-            else:
-                response = trigger
-            await message.channel.send('{}. {}.'.format(response.capitalize(), emoji_id))
+    randomNumber = randint(0, 5000)
+    if randomNumber == 1337:
+        words = message.content.split(' ')
+        num_words = len(words)
+        word_index = randint(0, num_words - 1)
+        await message.channel.send('{}. {}.'.format(words[word_index], emoji_id))
+    else:
+        for trigger in triggers:
+            if trigger in message.content.lower():
+                if trigger == "boll":
+                    response = "bollsport"
+                else:
+                    response = trigger
+                await message.channel.send('{}. {}.'.format(response.capitalize(), emoji_id))
 
-    for trigger in white_boy_rpg:
-        if trigger in message.content.lower():
-            await message.channel.send('{}. {}.'.format("White boy RPG", emoji_id))
-
+        for trigger in white_boy_rpg:
+            if trigger in message.content.lower():
+                await message.channel.send('{}. {}.'.format("White boy RPG", emoji_id))
 
 client.run(os.environ['secret'])
