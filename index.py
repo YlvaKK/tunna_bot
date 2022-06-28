6f01e527fbaf1d73dbe856b5cca463b4b0851e57
Ylva Karlberg <ylvakarlberg@gmail.com>
2022-06-28 22:59:28 +0200

    added white boy rpg support, fixed capitalization

--- index.py
+++ index.py
@@ -4,7 +4,8 @@ import os
 client = discord.Client()
 
 emoji_id = "<:hjelp:705824146046844982>" 
-triggers = {"bandy", "boll", "basket", "hockey", "mac", "macos", "linux", "ubuntu", "destiny", "elden ring", "kaffe", "stockholm", "njuta av livet"}
+triggers = {"bandy", "boll", "basket", "hockey", "mac", "macos", "linux", "ubuntu", "destiny", "elden ring", "kaffe", "stockholm", "njuta av livet", "paradox"}
+white_boy_rpg = {"witcher", "fallout","deus ex", "vampire: the masquerade", "divinity", "dragon age", "Star Wars: Knights"}
 response = ""
 
 @client.event
@@ -22,8 +23,11 @@ async def on_message(message):
                 response = "bollsport"
             else:
                 response = trigger
-            await message.channel.send('{}. {}.'.format(response, emoji_id))
+            await message.channel.send('{}. {}.'.format(response.capitalize(), emoji_id))
 
+    for trigger in white_boy_rpg:
+        if trigger in message.content.lower():
+            await message.channel.send('{}. {}.'.format("White boy RPG", emoji_id))
 
 
 client.run(os.environ['secret'])
